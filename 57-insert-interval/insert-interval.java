9e1aa12fdf25acecc1ecadaa89c5ecc88bbbc9c1
Gagan upadhyaya <upadhyayagagan2002@gmail.com>
2026-07-27 20:24:23 +0530

    Time: 1 ms (98.08%) | Memory: 46.7 MB (95.11%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -2,41 +2,41 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
 
         int n = intervals.length;
-
-        int [][] res = new int [n+1][];
-
-        for(int i=0; i<n ; i++){
-            res[i] = intervals[i];
-        }
-        res[n] = newInterval; 
-
-        Arrays.sort(res , (x,y) -> Integer.compare(x[0],y[0]));
-
         List<int[]>res1 = new ArrayList<>();
 
-        if(res.length == 0){
-            return new int [][]{};
+        boolean flag = false;
+        for(int i=0 ;i<n; i++){
+            if(!flag && newInterval[0] < intervals[i][0]){
+                res1.add(newInterval);
+                flag = true;
+            }
+            res1.add(new int []{intervals[i][0],intervals[i][1]});
+        }
+        if(!flag){
+            res1.add(newInterval);
         }
 
-        int s1 = res[0][0];
-        int e1 = res[0][1];
+        List<int[]>res2 = new ArrayList<>();
 
-        for(int i=1; i<res.length; i++){
-            int s2 = res[i][0];
-            int e2 = res[i][1];
+        int s1 = res1.get(0)[0];
+        int e1 = res1.get(0)[1];
+
+        for(int i=1; i<res1.size(); i++){
+            int s2 = res1.get(i)[0];
+            int e2 = res1.get(i)[1];
 
             if(e1 >= s2){
                 s1 = s1;
                 e1 = Math.max(e1,e2);
                 continue;
             }
-            res1.add(new int [] {s1,e1});
+            res2.add(new int [] {s1,e1});
             s1 = s2;
             e1 = e2;
         }
-        res1.add(new int [] {s1,e1});
+        res2.add(new int [] {s1,e1});
 
-        return res1.toArray(new int [res1.size()][]);
+        return res2.toArray(new int [res2.size()][]);
 
     }
 }
