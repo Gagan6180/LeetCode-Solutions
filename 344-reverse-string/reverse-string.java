ecad52af2690b05d3d6ae9961488c28299623c3a
Gagan upadhyaya <upadhyayagagan2002@gmail.com>
2026-07-29 23:34:06 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (97.08%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,13 +1,27 @@
 class Solution {
     public void reverseString(char[] s) {
-        Stack<Character>rev = new Stack<>();
+        // Stack<Character>rev = new Stack<>();
 
-        for(char letter: s){
-            rev.push(letter);
+        // for(char letter: s){
+        //     rev.push(letter);
+        // }
+
+        // for(int i=0 ; i<s.length ; i++){
+        //     s[i] = rev.pop();
+        // }
+
+        int i=0;
+        int j = s.length-1;
+
+        while(i<j){
+
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+
+            i++;
+            j--;
         }
 
-        for(int i=0 ; i<s.length ; i++){
-            s[i] = rev.pop();
-        }
     }
 }
