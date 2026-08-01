ca0137eb216eb3a339058f97d1295928d66775e5
Gagan upadhyaya <upadhyayagagan2002@gmail.com>
2026-08-01 22:14:20 +0530

    Time: 15 ms (61.46%) | Memory: 48.2 MB (75.59%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,29 +1,22 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n]; // Fixed: Create an array to store results
-        Stack<Integer> res = new Stack<>(); // The stack only holds the numbers
+        int n= nums.length;
+        int[]ans = new int[n];
+        Arrays.fill(ans,-1); //initially filling the ans array with -1;
+        Stack<Integer> st= new Stack<>();
 
-        // Loop twice (from 2*n-1 down to 0) to simulate the circular wrap-around
-        for (int i = 2 * n - 1; i >= 0; i--) {
-            int idx = i % n; // Get the actual index in the array
-
-            // Remove all smaller elements from the stack
-            while (!res.isEmpty() && res.peek() <= nums[idx]) {
-                res.pop();
+        for(int i =2*n -1; i>=0;i--){
+            int num=nums[i%n];
+            while(!st.isEmpty()&& st.peek()<=num){
+                st.pop();
             }
-
-            // Fixed: Assign answers to the 'result' array instead of the stack
-            if (res.isEmpty()) {
-                result[idx] = -1;
-            } else {
-                result[idx] = res.peek(); // Fixed typo: changed 'rev' to 'res'
+            //we are checking i < n for valid indecis in first exampty as we have increased the array till 5th index but valid and use ful insex are just 0 1 and 2 
+            if( i<n && !st.isEmpty()){
+                ans[i]=st.peek();
             }
-
-            // Push the current element onto the stack
-            res.push(nums[idx]);
+            st.push(num);
+            
         }
-
-        return result; // Return the correct intege
+        return ans;
     }
 }
