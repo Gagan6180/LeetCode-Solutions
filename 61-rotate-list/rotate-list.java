89bac48622c679f6d854788b9c27cdbcd5c74988
Gagan upadhyaya <upadhyayagagan2002@gmail.com>
2026-08-18 00:06:27 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (63.53%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -9,38 +9,37 @@
  * }
  */
 class Solution {
-    public ListNode rotateRight(ListNode head, int k) {
-        if (head == null || head.next == null || k == 0) {
-            return head;
+    private ListNode findNthNode(ListNode temp, int k){
+        int count = 1;
+
+        while(temp != null){
+            if(count == k) return temp;
+            count ++;
+            temp = temp.next;
         }
-        
-        // Find the length and the tail of the list
+        return temp;
+    }
+    public ListNode rotateRight(ListNode head, int k) {
         int len = 1;
         ListNode tail = head;
-        while (tail.next != null) {
+
+        if(head == null || k == 0) return head;
+
+        while(tail.next != null){
             tail = tail.next;
-            len++;
+            len ++;
         }
-        
-        // Optimize k if it is larger than the list length
-        k = k % len;
-        if (k == 0) {
-            return head;
-        }
-        
-        // Make the list circular
+        if(k % len == 0) return head;
+
+        k = k%len;
+
         tail.next = head;
-        
-        // Find the new tail: (len - k - 1) steps from the head
-        ListNode newTail = head;
-        for (int i = 0; i < len - k - 1; i++) {
-            newTail = newTail.next;
-        }
-        
-        // The new head is next to the new tail
-        ListNode newHead = newTail.next;
-        newTail.next = null;
-        
-        return newHead;
+
+        ListNode newLastNode = findNthNode(head,len-k);
+        head = newLastNode.next;
+        newLastNode.next = null;
+
+        return head;
+
     }
 }
