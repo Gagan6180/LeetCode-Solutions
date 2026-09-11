c3df77b3428981ecd32bdc07a978fcc4dd9c7d5f
Gagan upadhyaya <upadhyayagagan2002@gmail.com>
2026-09-11 21:55:29 +0530

    Time: 3 ms (20.95%) | Memory: 47.3 MB (33.84%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -4,8 +4,8 @@ class Solution {
         int minEnd = nums[0];
         int ans = nums[0];
 
-        for(int i=1;i<nums.length;i++){
-            
+        for(int i=1;i<=nums.length-1;i++){
+
             int v1 = nums[i];
             int v2 = nums[i]*maxEnd;
             int v3 = nums[i]*minEnd;
