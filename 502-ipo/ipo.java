188cb3d4b23dfcaf11642b25bf4b02d97e38ec37
Gagan upadhyaya <upadhyayagagan2002@gmail.com>
2026-09-19 21:47:47 +0530

    Time: 119 ms (31.94%) | Memory: 131.9 MB (90.92%) - LeetSync

---
+++ 502-ipo/ipo.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int [][] merge = new int [capital.length][2];
+
+        for(int i=0; i<capital.length; i++){
+            merge[i][0] = capital[i];
+            merge[i][1] = profits[i];
+        }
+
+        Arrays.sort(merge,(a,b) -> Integer.compare(a[0],b[0]));
+
+        PriorityQueue<Integer>maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        int i=0;
+        while(k > 0){
+            while(i < profits.length && merge[i][0] <= w){
+                maxHeap.add(merge[i][1]);
+                i++;
+            }
+            if(maxHeap.isEmpty()){
+                break;
+            }
+            w += maxHeap.poll();
+            k--;
+        }
+        return w;
+    }
+}
