8da9cc01fd61a2e5d8e9fa2ed758f7858dbe07d0
Gagan upadhyaya <upadhyayagagan2002@gmail.com>
2026-09-27 00:55:12 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (70.03%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,24 @@
+class Solution {
+    public double myPow(double x, int n) {
+       double ans =1;
+       long N = n;
+
+       if(N<0){
+        N = -N;
+       }
+
+       while(N > 0){
+        if(N%2 == 1){
+            ans = ans*x;
+            N=N-1;
+        }else{
+            x=x*x;
+            N=N/2;
+        }
+       }
+       if(n<0){
+        ans = 1.0/ans;
+       }
+       return ans;
+    }
+}
